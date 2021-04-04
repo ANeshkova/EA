2de15a4cf15a3ab38aa72f7a7726f1245fbdbea1
ANeshkova <anitowurth@abv.bg>
2021-04-04 10:11:40 +0100

    Added WordRepository

---
+++ src/main/java/bg/an/englishacademy/repository/WordRepository.java
@@ -0,0 +1,9 @@
+package bg.an.englishacademy.repository;
+
+import bg.an.englishacademy.model.entity.WordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WordRepository extends JpaRepository<WordEntity, Long> {
+}
