f3edd9f8e2d995d08e35a6ebd0592e3feb045b32
ANeshkova <anitowurth@abv.bg>
2021-04-04 10:17:42 +0100

    Added LessonRepository

---
+++ src/main/java/bg/an/englishacademy/repository/LessonRepository.java
@@ -0,0 +1,9 @@
+package bg.an.englishacademy.repository;
+
+import bg.an.englishacademy.model.entity.LessonEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LessonRepository extends JpaRepository<LessonEntity, Long> {
+}
