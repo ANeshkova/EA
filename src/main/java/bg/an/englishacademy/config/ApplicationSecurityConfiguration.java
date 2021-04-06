32d95a5e4d335c2b8ad40d7c94a8809c10240647
ANeshkova <anitowurth@abv.bg>
2021-04-06 12:51:32 +0100

    Initial security implementation

--- src/main/java/bg/an/englishacademy/config/ApplicationSecurityConfiguration.java
+++ src/main/java/bg/an/englishacademy/config/ApplicationSecurityConfiguration.java
@@ -1,4 +1,35 @@
 package bg.an.englishacademy.config;
 
-public class ApplicationSecurityConfiguration {
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors().disable()
+                .csrf().disable().
+                authorizeRequests().
+                // allow access to static resources to anyone
+                        requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll(). //img must be images!!!
+                //antMatchers("/js/**", "/css/**", "/img/**").permitAll().
+                // allow access to index, user login and registration to anyone
+                        antMatchers("/", "/users/login", "/users/register", "/error").permitAll().
+                // protect all other pages
+                        antMatchers("/**").authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 }
