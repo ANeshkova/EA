b3aa9cc1d5a728d85d21b41d22ed3c9e0d4b4f0a
ANeshkova <anitowurth@abv.bg>
2021-04-07 19:27:15 +0100

    Added configuration

--- src/main/java/bg/an/englishacademy/config/ApplicationSecurityConfiguration.java
+++ src/main/java/bg/an/englishacademy/config/ApplicationSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package bg.an.englishacademy.config;
 
+import bg.an.englishacademy.service.impl.MyUserDetailsServiceImpl;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,11 +8,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final MyUserDetailsServiceImpl myUserDetailsServiceImpl;
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfiguration(MyUserDetailsServiceImpl myUserDetailsServiceImpl, PasswordEncoder passwordEncoder) {
+        this.myUserDetailsServiceImpl = myUserDetailsServiceImpl;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -25,11 +36,28 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 // allow access to index, user login and registration to anyone
                         antMatchers("/", "/users/login", "/users/register", "/error").permitAll().
                 // protect all other pages
-                        antMatchers("/**").authenticated();
+                        antMatchers("/**").authenticated().
+        and().
+                // configure login with HTML form
+                        formLogin().
+                // our login page will be served by the controller with mapping /users/login
+                        loginPage("/users/login").
+                // the name of the user name input field in OUR login form is username (optional)
+                        usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
+                //usernameParameter("username").
+                // the name of the user password input field in OUR login form is password (optional)
+                        passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
+                //passwordParameter("password").
+                // on login success redirect here
+                        defaultSuccessUrl("/").
+                // on login failure redirect here
+                        failureForwardUrl("/users/login-error");
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.
+                userDetailsService(myUserDetailsServiceImpl).
+                passwordEncoder(passwordEncoder);
     }
 }
