a77e70ad63b9302840dcbf9d6be1baaf7c64cd1e
ANeshkova <anitowurth@abv.bg>
2021-04-08 21:22:38 +0100

    Added seedRolesInDB implementation

--- src/main/java/bg/an/englishacademy/service/impl/RoleServiceImpl.java
+++ src/main/java/bg/an/englishacademy/service/impl/RoleServiceImpl.java
@@ -1,6 +1,23 @@
 package bg.an.englishacademy.service.impl;
 
+import bg.an.englishacademy.model.entity.RoleEntity;
+import bg.an.englishacademy.repository.RoleRepository;
 import bg.an.englishacademy.service.RoleService;
 
 public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void seedRolesInDB() {
+        if (this.roleRepository.count() == 0) {
+            this.roleRepository.saveAndFlush(new RoleEntity("USER"));
+            this.roleRepository.saveAndFlush(new RoleEntity("ADMIN"));
+            this.roleRepository.saveAndFlush(new RoleEntity("ROOT"));
+        }
+    }
 }
