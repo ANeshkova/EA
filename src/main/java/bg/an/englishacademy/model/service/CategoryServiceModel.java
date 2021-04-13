b0f1edf209185e015a880dfe6a4fc675f85aa964
ANeshkova <anitowurth@abv.bg>
2021-04-13 17:48:51 +0300

    Added CategoryServiceModel

---
+++ src/main/java/bg/an/englishacademy/model/service/CategoryServiceModel.java
@@ -0,0 +1,26 @@
+package bg.an.englishacademy.model.service;
+
+public class CategoryServiceModel extends BaseServiceModel {
+
+    private String name;
+    private String imageUrl;
+
+    public CategoryServiceModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
