dedd6fa246cd2f01849632351dac3ade81826f63
ANeshkova <anitowurth@abv.bg>
2021-04-15 20:49:00 +0300

    Added logic to the register method

--- src/main/java/bg/an/englishacademy/web/controllers/UserController.java
+++ src/main/java/bg/an/englishacademy/web/controllers/UserController.java
@@ -1,6 +1,9 @@
 package bg.an.englishacademy.web.controllers;
 
+import bg.an.englishacademy.model.binding.UserRegisterBindingModel;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/register")
-    public String register() {
+    @PreAuthorize("isAnonymous()")
+    public String register(Model model) {
+
+        if (!model.containsAttribute("userRegisterBindingModel")) {
+            model.addAttribute("userRegisterBindingModel", new UserRegisterBindingModel());
+        }
+
         return "users/register";
     }
 
