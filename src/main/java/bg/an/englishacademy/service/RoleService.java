940107222b077c70d62b53bdfd192656c2fe600e
ANeshkova <anitowurth@abv.bg>
2021-04-17 14:57:40 +0300

    Added "findAllRoles" in RoleService

--- src/main/java/bg/an/englishacademy/service/RoleService.java
+++ src/main/java/bg/an/englishacademy/service/RoleService.java
@@ -1,6 +1,13 @@
 package bg.an.englishacademy.service;
 
+import bg.an.englishacademy.model.service.RoleServiceModel;
+
+import java.util.Set;
+
 public interface RoleService {
 
     void seedRolesInDB();
+
+    Set<RoleServiceModel> findAllRoles();
+
 }
