6f1ec513a7975dfe057cb4d09f65a64df19581d9
ANeshkova <anitowurth@abv.bg>
2021-04-18 15:01:17 +0300

    Added "registerUser" in UserService

--- src/main/java/bg/an/englishacademy/service/UserService.java
+++ src/main/java/bg/an/englishacademy/service/UserService.java
@@ -1,4 +1,8 @@
 package bg.an.englishacademy.service;
 
+import bg.an.englishacademy.model.service.UserServiceModel;
+
 public interface UserService {
+
+    void registerUser(UserServiceModel userServiceModel);
 }
