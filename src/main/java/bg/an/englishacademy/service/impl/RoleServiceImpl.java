c67e6317f488d6aac45eee12deff3c6cc92a0aae
ANeshkova <anitowurth@abv.bg>
2021-04-19 13:28:37 +0300

    Implemented "findAllRoles" method

--- src/main/java/bg/an/englishacademy/service/impl/RoleServiceImpl.java
+++ src/main/java/bg/an/englishacademy/service/impl/RoleServiceImpl.java
@@ -1,15 +1,24 @@
 package bg.an.englishacademy.service.impl;
 
 import bg.an.englishacademy.model.entity.RoleEntity;
+import bg.an.englishacademy.model.service.RoleServiceModel;
 import bg.an.englishacademy.repository.RoleRepository;
 import bg.an.englishacademy.service.RoleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final ModelMapper modelMapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, ModelMapper modelMapper) {
         this.roleRepository = roleRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -19,5 +28,14 @@ public class RoleServiceImpl implements RoleService {
             this.roleRepository.saveAndFlush(new RoleEntity("ADMIN"));
             this.roleRepository.saveAndFlush(new RoleEntity("ROOT"));
         }
+    }
+
+    @Override
+    public Set<RoleServiceModel> findAllRoles() {
+        return this.roleRepository
+                .findAll()
+                .stream()
+                .map(role -> this.modelMapper.map(role, RoleServiceModel.class))
+                .collect(Collectors.toSet());
     }
 }
