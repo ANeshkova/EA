4794cc0307786cd17d0e6a351b7048f01a2d5b88
ANeshkova <anitowurth@abv.bg>
2021-04-19 13:30:11 +0300

    Added BaseController

---
+++ src/main/java/bg/an/englishacademy/web/controllers/BaseController.java
@@ -0,0 +1,20 @@
+package bg.an.englishacademy.web.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+
+public abstract class BaseController {
+
+    public ModelAndView view(String viewName, ModelAndView modelAndView) {
+        modelAndView.setViewName(viewName);
+
+        return modelAndView;
+    }
+
+    public ModelAndView view(String viewName) {
+        return this.view(viewName, new ModelAndView());
+    }
+
+    public String redirect(String url) {
+        return ("redirect:" + url);
+    }
+}
