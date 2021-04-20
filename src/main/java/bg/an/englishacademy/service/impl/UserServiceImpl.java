e2ba269669b4d5dca5b15f0e0a4315434432f791
ANeshkova <anitowurth@abv.bg>
2021-04-20 15:32:27 +0300

    Implemented "registerUser" method

--- src/main/java/bg/an/englishacademy/service/impl/UserServiceImpl.java
+++ src/main/java/bg/an/englishacademy/service/impl/UserServiceImpl.java
@@ -1,6 +1,51 @@
 package bg.an.englishacademy.service.impl;
 
+import bg.an.englishacademy.model.entity.UserEntity;
+import bg.an.englishacademy.model.service.UserServiceModel;
+import bg.an.englishacademy.repository.UserRepository;
+import bg.an.englishacademy.service.RoleService;
 import bg.an.englishacademy.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashSet;
+
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final MyUserDetailsServiceImpl userDetailsService;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, RoleService roleService,
+                           MyUserDetailsServiceImpl userDetailsService, ModelMapper modelMapper,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleService = roleService;
+        this.userDetailsService = userDetailsService;
+        this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void registerUser(UserServiceModel userServiceModel) {
+
+        this.roleService.seedRolesInDB();
+
+        if (this.userRepository.count() == 0) {
+            userServiceModel.setRoles(this.roleService.findAllRoles());
+        } else {
+            userServiceModel.setRoles(new LinkedHashSet<>());
+            userServiceModel.getRoles().add(this.roleService.findByRole("USER"));
+        }
+
+        UserEntity userEntity = this.modelMapper.map(userServiceModel, UserEntity.class);
+        userEntity.setPassword(this.passwordEncoder.encode(userEntity.getPassword()));
+        userEntity.setWords(new LinkedHashSet<>());
+
+        this.userRepository.save(userEntity);
+    }
 }
