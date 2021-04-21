387070c0f4dedfe8b8807ddbd85a5b3dfa537056
ANeshkova <anitowurth@abv.bg>
2021-04-21 16:10:30 +0300

    Added UserValidationService

---
+++ src/main/java/bg/an/englishacademy/validation/UserValidationService.java
@@ -0,0 +1,11 @@
+package bg.an.englishacademy.validation;
+
+import bg.an.englishacademy.model.binding.UserRegisterBindingModel;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+public interface UserValidationService {
+
+    boolean userRegisterHasErrors(UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult, RedirectAttributes redirectAttributes);
+}
