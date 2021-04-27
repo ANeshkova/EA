3f2b01f19407d745e64eeea9b624276eabb1cb19
ANeshkova <anitowurth@abv.bg>
2021-04-27 18:34:16 +0300

    Added UserEditBindingModel

---
+++ src/main/java/bg/an/englishacademy/model/binding/UserEditBindingModel.java
@@ -0,0 +1,66 @@
+package bg.an.englishacademy.model.binding;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserEditBindingModel {
+
+    private String username;
+    private String email;
+    private String oldPassword;
+    private String password;
+    private String confirmNewPassword;
+
+    public UserEditBindingModel() {
+    }
+
+    @NotBlank(message = "Username can not be empty string")
+    @Size(min = 3, max = 20, message = "Username length must be between 3 and 20 characters")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @NotBlank(message = "Email can not be empty string")
+    @Size(min = 5, max = 40, message = "Email length must be between 5 and 40 characters")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @NotBlank(message = "Password can not be empty")
+    @Size(min = 5, max = 20, message = "Password length must be between 5 and 20 characters")
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    @NotBlank(message = "Password can not be empty")
+    @Size(min = 5, max = 20, message = "Password length must be between 5 and 20 characters")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @NotBlank(message = "Password can not be empty")
+    @Size(min = 5, max = 20, message = "Password length must be between 5 and 20 characters")
+    public String getConfirmNewPassword() {
+        return confirmNewPassword;
+    }
+
+    public void setConfirmNewPassword(String confirmNewPassword) {
+        this.confirmNewPassword = confirmNewPassword;
+    }
+}
