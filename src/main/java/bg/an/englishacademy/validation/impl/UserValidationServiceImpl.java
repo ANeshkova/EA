26f191f69f5f3d004110580a94b58211b4151e8b
ANeshkova <anitowurth@abv.bg>
2021-04-28 17:47:12 +0300

    Implemented "userEditHasErrors" in UserValidationServiceImpl

--- src/main/java/bg/an/englishacademy/validation/impl/UserValidationServiceImpl.java
+++ src/main/java/bg/an/englishacademy/validation/impl/UserValidationServiceImpl.java
@@ -1,5 +1,6 @@
 package bg.an.englishacademy.validation.impl;
 
+import bg.an.englishacademy.model.binding.UserEditBindingModel;
 import bg.an.englishacademy.model.binding.UserRegisterBindingModel;
 import bg.an.englishacademy.service.UserService;
 import bg.an.englishacademy.validation.UserValidationService;
@@ -41,6 +42,40 @@ public class UserValidationServiceImpl implements UserValidationService {
 
         if (this.userService.emailExists(userRegisterBindingModel.getEmail())) {
             redirectAttributes.addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel);
+            redirectAttributes.addFlashAttribute("emailAlreadyExists", true);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean userEditHasErrors(UserEditBindingModel userEditBindingModel, BindingResult bindingResult,
+                                     RedirectAttributes redirectAttributes, String currentUsername, String currentEmail) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("userEditBindingModel", userEditBindingModel);
+            redirectAttributes.addFlashAttribute(
+                    "org.springframework.validation.BindingResult.userEditBindingModel", bindingResult);
+            return true;
+        }
+
+        if (!userEditBindingModel.getPassword().equals(userEditBindingModel.getConfirmNewPassword())){
+            redirectAttributes.addFlashAttribute("userEditBindingModel", userEditBindingModel);
+            redirectAttributes.addFlashAttribute("passwordsDontMatch", true);
+            return true;
+        }
+
+        if (this.userService.usernameExists(userEditBindingModel.getUsername())
+                && !userEditBindingModel.getUsername().equalsIgnoreCase(currentUsername)) {
+            redirectAttributes.addFlashAttribute("userEditBindingModel", userEditBindingModel);
+            redirectAttributes.addFlashAttribute("usernameAlreadyExists", true);
+            return true;
+        }
+
+        if (this.userService.emailExists(userEditBindingModel.getEmail())
+                && !userEditBindingModel.getEmail().equalsIgnoreCase(currentEmail)) {
+            redirectAttributes.addFlashAttribute("userEditBindingModel", userEditBindingModel);
             redirectAttributes.addFlashAttribute("emailAlreadyExists", true);
             return true;
         }
