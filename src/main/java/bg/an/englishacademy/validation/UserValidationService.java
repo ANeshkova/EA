80527ee51a433914bf30448056d44ed28c81bb09
ANeshkova <anitowurth@abv.bg>
2021-04-28 17:46:33 +0300

    Added "userEditHasErrors" in UserValidationService

--- src/main/java/bg/an/englishacademy/validation/UserValidationService.java
+++ src/main/java/bg/an/englishacademy/validation/UserValidationService.java
@@ -1,5 +1,6 @@
 package bg.an.englishacademy.validation;
 
+import bg.an.englishacademy.model.binding.UserEditBindingModel;
 import bg.an.englishacademy.model.binding.UserRegisterBindingModel;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -8,4 +9,7 @@ public interface UserValidationService {
 
     boolean userRegisterHasErrors(UserRegisterBindingModel userRegisterBindingModel,
                                   BindingResult bindingResult, RedirectAttributes redirectAttributes);
+
+    boolean userEditHasErrors(UserEditBindingModel userEditBindingModel, BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes, String currentUsername, String currentEmail);
 }
