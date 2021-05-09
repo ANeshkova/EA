6cd21b1dcd10d75979b5417882ee8de2dcc3a2a3
ANeshkova <anitowurth@abv.bg>
2021-05-10 01:49:46 +0300

    Added "addCategory" in CategoryService

--- src/main/java/bg/an/englishacademy/service/CategoryService.java
+++ src/main/java/bg/an/englishacademy/service/CategoryService.java
@@ -1,7 +1,11 @@
 package bg.an.englishacademy.service;
 
+import bg.an.englishacademy.model.service.CategoryServiceModel;
 
 public interface CategoryService {
 
     boolean categoryNameExists(String name);
+
+    CategoryServiceModel addCategory(CategoryServiceModel categoryServiceModel);
+
 }
