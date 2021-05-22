2b835d93083063a5ae65a118d8f5552a4dde09f5
ANeshkova <anitowurth@abv.bg>
2021-05-22 16:16:36 +0300

    Added WordController

---
+++ src/main/java/bg/an/englishacademy/web/controllers/WordController.java
@@ -0,0 +1,9 @@
+package bg.an.englishacademy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/words")
+public class WordController extends BaseController {
+}
