c7e0dcd58e9710f5eae49c48f124f439a9c55aec
ANeshkova <anitowurth@abv.bg>
2021-05-22 16:17:06 +0300

    Added WordServiceImpl

---
+++ src/main/java/bg/an/englishacademy/service/impl/WordServiceImpl.java
@@ -0,0 +1,8 @@
+package bg.an.englishacademy.service.impl;
+
+import bg.an.englishacademy.service.WordService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WordServiceImpl implements WordService {
+}
