7aac28a8fd21ec3d13922de090145130e4c6c9fd
ANeshkova <anitowurth@abv.bg>
2021-05-23 14:17:48 +0300

    Added WordValidationServiceImpl

---
+++ src/main/java/bg/an/englishacademy/validation/impl/WordValidationServiceImpl.java
@@ -0,0 +1,8 @@
+package bg.an.englishacademy.validation.impl;
+
+import bg.an.englishacademy.validation.WordValidationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WordValidationServiceImpl implements WordValidationService {
+}
