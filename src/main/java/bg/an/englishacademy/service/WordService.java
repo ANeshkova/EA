c07626b484a84e3372f4f61b494b3b45a0b2ef25
ANeshkova <anitowurth@abv.bg>
2021-05-24 23:51:54 +0300

    Added "addWord" in WordService

--- src/main/java/bg/an/englishacademy/service/WordService.java
+++ src/main/java/bg/an/englishacademy/service/WordService.java
@@ -1,4 +1,8 @@
 package bg.an.englishacademy.service;
 
+import bg.an.englishacademy.model.service.WordServiceModel;
+
 public interface WordService {
+
+    WordServiceModel addWord(WordServiceModel wordServiceModel);
 }
