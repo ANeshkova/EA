f40b514eb6fe0db1a7c910378bffb11419c42401
ANeshkova <anitowurth@abv.bg>
2021-05-24 23:53:06 +0300

    Implemented "addWord" in WordServiceImpl

--- src/main/java/bg/an/englishacademy/service/impl/WordServiceImpl.java
+++ src/main/java/bg/an/englishacademy/service/impl/WordServiceImpl.java
@@ -1,8 +1,26 @@
 package bg.an.englishacademy.service.impl;
 
+import bg.an.englishacademy.model.entity.WordEntity;
+import bg.an.englishacademy.model.service.WordServiceModel;
+import bg.an.englishacademy.repository.WordRepository;
 import bg.an.englishacademy.service.WordService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WordServiceImpl implements WordService {
+
+    private final WordRepository wordRepository;
+    private final ModelMapper modelMapper;
+
+    public WordServiceImpl(WordRepository wordRepository, ModelMapper modelMapper) {
+        this.wordRepository = wordRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public WordServiceModel addWord(WordServiceModel wordServiceModel) {
+        this.wordRepository.save(modelMapper.map(wordServiceModel, WordEntity.class));
+        return wordServiceModel;
+    }
 }
