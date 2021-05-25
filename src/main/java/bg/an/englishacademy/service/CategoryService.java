d1f946164784a001774f46d50357a287d8156566
ANeshkova <anitowurth@abv.bg>
2021-05-25 20:57:00 +0300

    Added "findAllCategoryNames" in CategoryService

--- src/main/java/bg/an/englishacademy/service/CategoryService.java
+++ src/main/java/bg/an/englishacademy/service/CategoryService.java
@@ -2,10 +2,13 @@ package bg.an.englishacademy.service;
 
 import bg.an.englishacademy.model.service.CategoryServiceModel;
 
+import java.util.List;
+
 public interface CategoryService {
 
     boolean categoryNameExists(String name);
 
     CategoryServiceModel addCategory(CategoryServiceModel categoryServiceModel);
 
+    List<String> findAllCategoryNames();
 }
