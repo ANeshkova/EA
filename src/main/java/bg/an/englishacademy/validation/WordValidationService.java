115c6ab6f5727b5491409e414fa6690201ccde12
ANeshkova <anitowurth@abv.bg>
2021-05-27 18:01:08 +0300

    Added "wordAddHasErrors" in WordValidationService

--- src/main/java/bg/an/englishacademy/validation/WordValidationService.java
+++ src/main/java/bg/an/englishacademy/validation/WordValidationService.java
@@ -1,4 +1,11 @@
 package bg.an.englishacademy.validation;
 
+import bg.an.englishacademy.model.binding.WordAddBindingModel;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 public interface WordValidationService {
+
+    boolean wordAddHasErrors(WordAddBindingModel wordAddBindingModel, BindingResult bindingResult, RedirectAttributes redirectAttributes);
+
 }
