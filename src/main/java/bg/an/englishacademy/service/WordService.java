4d441410d036347861365ec7c9baebb3fba065a9
ANeshkova <anitowurth@abv.bg>
2021-05-29 00:30:19 +0300

    Added "findAllWords" in WordService

--- src/main/java/bg/an/englishacademy/service/WordService.java
+++ src/main/java/bg/an/englishacademy/service/WordService.java
@@ -2,9 +2,13 @@ package bg.an.englishacademy.service;
 
 import bg.an.englishacademy.model.service.WordServiceModel;
 
+import java.util.List;
+
 public interface WordService {
 
     WordServiceModel addWord(WordServiceModel wordServiceModel);
 
     boolean englishWordExistsInCategory(String englishWord, String categoryName);
+
+    List<WordServiceModel> findAllWords();
 }
