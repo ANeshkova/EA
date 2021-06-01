944c1d3bdc11df2264fe929ca2cb8a7304a7f461
ANeshkova <anitowurth@abv.bg>
2021-06-02 01:00:50 +0300

    Added "wordEditHasErrors" in WordValidationService

--- src/main/java/bg/an/englishacademy/validation/WordValidationService.java
+++ src/main/java/bg/an/englishacademy/validation/WordValidationService.java
@@ -1,6 +1,7 @@
 package bg.an.englishacademy.validation;
 
 import bg.an.englishacademy.model.binding.WordAddBindingModel;
+import bg.an.englishacademy.model.binding.WordEditBindingModel;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -8,4 +9,6 @@ public interface WordValidationService {
 
     boolean wordAddHasErrors(WordAddBindingModel wordAddBindingModel, BindingResult bindingResult, RedirectAttributes redirectAttributes);
 
+    boolean wordEditHasErrors(WordEditBindingModel wordEditBindingModel, BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes, Long id);
 }
