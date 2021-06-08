ffd5b61720e264cf632e5ff602cd586b1ade47bc
ANeshkova <anitowurth@abv.bg>
2021-06-08 22:37:53 +0300

    Added CategoryViewModel

---
+++ src/main/java/bg/an/englishacademy/model/view/CategoryViewModel.java
@@ -0,0 +1,35 @@
+package bg.an.englishacademy.model.view;
+
+public class CategoryViewModel {
+
+    private Long id;
+    private String name;
+    private String imageUrl;
+
+    public CategoryViewModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
