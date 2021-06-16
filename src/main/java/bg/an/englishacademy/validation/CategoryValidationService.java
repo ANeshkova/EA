ca2b96aecb9006d86578be24b7f996ad2bf21a00
ANeshkova <anitowurth@abv.bg>
2021-06-16 22:18:40 +0300

    Added "categoryEditHasErrors" in CategoryValidationService

--- src/main/java/bg/an/englishacademy/validation/CategoryValidationService.java
+++ src/main/java/bg/an/englishacademy/validation/CategoryValidationService.java
@@ -1,10 +1,14 @@
 package bg.an.englishacademy.validation;
 
 import bg.an.englishacademy.model.binding.CategoryAddBindingModel;
+import bg.an.englishacademy.model.binding.CategoryEditBindingModel;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 public interface CategoryValidationService {
 
     boolean categoryAddHasErrors(CategoryAddBindingModel categoryAddBindingModel, BindingResult bindingResult, RedirectAttributes redirectAttributes);
+
+    boolean categoryEditHasErrors(CategoryEditBindingModel categoryEditBindingModel, BindingResult bindingResult,
+                                  RedirectAttributes redirectAttributes, Long id);
 }
