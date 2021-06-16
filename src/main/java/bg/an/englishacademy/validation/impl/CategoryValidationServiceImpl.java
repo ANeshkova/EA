761b5c1ec0f11ba83ce507848f16a42880a220b6
ANeshkova <anitowurth@abv.bg>
2021-06-16 22:19:15 +0300

    Implemented "categoryEditHasErrors" in CategoryValidationServiceImpl

--- src/main/java/bg/an/englishacademy/validation/impl/CategoryValidationServiceImpl.java
+++ src/main/java/bg/an/englishacademy/validation/impl/CategoryValidationServiceImpl.java
@@ -1,6 +1,8 @@
 package bg.an.englishacademy.validation.impl;
 
 import bg.an.englishacademy.model.binding.CategoryAddBindingModel;
+import bg.an.englishacademy.model.binding.CategoryEditBindingModel;
+import bg.an.englishacademy.model.service.CategoryServiceModel;
 import bg.an.englishacademy.service.CategoryService;
 import bg.an.englishacademy.validation.CategoryValidationService;
 import org.springframework.stereotype.Service;
@@ -33,6 +35,27 @@ public class CategoryValidationServiceImpl implements CategoryValidationService 
 
         if (this.categoryService.categoryNameExists(categoryAddBindingModel.getName())) {
             redirectAttributes.addFlashAttribute("categoryAddBindingModel", categoryAddBindingModel);
+            redirectAttributes.addFlashAttribute("categoryNameAlreadyExists", true);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean categoryEditHasErrors(CategoryEditBindingModel categoryEditBindingModel, BindingResult bindingResult,
+                                         RedirectAttributes redirectAttributes, Long id) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("categoryEditBindingModel", categoryEditBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.categoryEditBindingModel", bindingResult);
+            return true;
+        }
+
+        CategoryServiceModel currentCategory = this.categoryService.findCategoryById(id);
+
+        if (this.categoryService.categoryNameExists(categoryEditBindingModel.getName())
+                && !categoryEditBindingModel.getName().equalsIgnoreCase(currentCategory.getName())) {
+            redirectAttributes.addFlashAttribute("categoryEditBindingModel", categoryEditBindingModel);
             redirectAttributes.addFlashAttribute("categoryNameAlreadyExists", true);
             return true;
         }
