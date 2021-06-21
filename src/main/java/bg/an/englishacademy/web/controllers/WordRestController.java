9ce08e90c3130ededfdc1abaf2731a45e12511fc
ANeshkova <anitowurth@abv.bg>
2021-06-21 23:44:31 +0300

    Added WordRestController

---
+++ src/main/java/bg/an/englishacademy/web/controllers/WordRestController.java
@@ -0,0 +1,7 @@
+package bg.an.englishacademy.web.controllers;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/words")
+public class WordRestController {
+}
