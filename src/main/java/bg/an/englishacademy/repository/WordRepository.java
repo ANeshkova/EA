606d74c4f46f187f7062b66c7f24778b6b68e79d
ANeshkova <anitowurth@abv.bg>
2021-06-22 20:51:46 +0300

    Added "findAllByCategory_Name" in WordRepository

--- src/main/java/bg/an/englishacademy/repository/WordRepository.java
+++ src/main/java/bg/an/englishacademy/repository/WordRepository.java
@@ -4,10 +4,13 @@ import bg.an.englishacademy.model.entity.WordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WordRepository extends JpaRepository<WordEntity, Long> {
 
     Optional<WordEntity> findByEnglishAndCategory_Name(String word, String categoryName);
+
+    List<WordEntity> findAllByCategory_Name(String category);
 }
