6d04d9f9df1f70183396c1e7772246350136d6f5
ANeshkova <anitowurth@abv.bg>
2021-06-24 20:01:44 +0300

    Added GET find all words in WordRestController

--- src/main/java/bg/an/englishacademy/web/controllers/WordRestController.java
+++ src/main/java/bg/an/englishacademy/web/controllers/WordRestController.java
@@ -1,7 +1,44 @@
 package bg.an.englishacademy.web.controllers;
+
+import bg.an.englishacademy.model.service.UserServiceModel;
+import bg.an.englishacademy.model.service.WordServiceModel;
+import bg.an.englishacademy.model.view.WordViewModel;
+import bg.an.englishacademy.service.UserService;
+import bg.an.englishacademy.service.WordService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/words")
 public class WordRestController {
+
+    private final WordService wordService;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    public WordRestController(WordService wordService, UserService userService, ModelMapper modelMapper) {
+        this.wordService = wordService;
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<List<WordViewModel>> findAll() {
+        return ResponseEntity
+                .ok()
+                .body(this.wordService
+                        .findAllWords()
+                        .stream()
+                        .map(w -> this.modelMapper.map(w, WordViewModel.class))
+                        .collect(Collectors.toList()));
+    }
 }
