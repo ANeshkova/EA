a726730726691f7bed8cd7a763c66e364d6fde3c
ANeshkova <anitowurth@abv.bg>
2021-07-03 02:58:23 +0300

    Added LessonViewModel

---
+++ src/main/java/bg/an/englishacademy/model/view/LessonViewModel.java
@@ -0,0 +1,55 @@
+package bg.an.englishacademy.model.view;
+
+import java.time.LocalDate;
+
+public class LessonViewModel {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String videoUrl;
+    private LocalDate addedOn;
+
+    public LessonViewModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public LocalDate getAddedOn() {
+        return addedOn;
+    }
+
+    public void setAddedOn(LocalDate addedOn) {
+        this.addedOn = addedOn;
+    }
+}
