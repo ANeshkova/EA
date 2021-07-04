60942182dee7a8da17dfe3f0c26a774868ad5134
ANeshkova <anitowurth@abv.bg>
2021-07-04 21:00:12 +0300

    Added "findByTitle" in LessonRepository

--- src/main/java/bg/an/englishacademy/repository/LessonRepository.java
+++ src/main/java/bg/an/englishacademy/repository/LessonRepository.java
@@ -4,6 +4,10 @@ import bg.an.englishacademy.model.entity.LessonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LessonRepository extends JpaRepository<LessonEntity, Long> {
+
+    Optional<LessonEntity> findByTitle(String title);
 }
