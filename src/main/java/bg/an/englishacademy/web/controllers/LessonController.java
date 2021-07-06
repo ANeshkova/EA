d8f4ee381ae209d89d3f77603a34207824f27c25
ANeshkova <anitowurth@abv.bg>
2021-07-06 19:16:36 +0300

    Added GET add lesson

--- src/main/java/bg/an/englishacademy/web/controllers/LessonController.java
+++ src/main/java/bg/an/englishacademy/web/controllers/LessonController.java
@@ -1,7 +1,22 @@
 package bg.an.englishacademy.web.controllers;
+
+import bg.an.englishacademy.model.binding.LessonAddBindingModel;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/lessons")
 public class LessonController extends BaseController{
+
+    @GetMapping("/add")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String addLesson(Model model) {
+
+        if (!model.containsAttribute("lessonAddBindingModel")) {
+            model.addAttribute("lessonAddBindingModel", new LessonAddBindingModel());
+        }
+
+        return "lessons/lesson-add";
+    }
 }
