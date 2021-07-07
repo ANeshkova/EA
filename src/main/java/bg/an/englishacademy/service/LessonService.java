0fc61af32c4103d1b29632f49113b99875d590ef
ANeshkova <anitowurth@abv.bg>
2021-07-07 21:19:41 +0300

    Added "addLesson" in LessonService

--- src/main/java/bg/an/englishacademy/service/LessonService.java
+++ src/main/java/bg/an/englishacademy/service/LessonService.java
@@ -1,3 +1,7 @@
 package bg.an.englishacademy.service;
+
+import bg.an.englishacademy.model.service.LessonServiceModel;
+
 public interface LessonService {
+    LessonServiceModel addLesson(LessonServiceModel lessonServiceModel);
 }
