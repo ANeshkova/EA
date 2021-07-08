2166eba006cadaccd54771f4b208fc7c387cd944
ANeshkova <anitowurth@abv.bg>
2021-07-08 10:13:33 +0300

    Added "lessonAddHasErrors" in LessonValidationService

--- src/main/java/bg/an/englishacademy/validation/LessonValidationService.java
+++ src/main/java/bg/an/englishacademy/validation/LessonValidationService.java
@@ -1,3 +1,11 @@
 package bg.an.englishacademy.validation;
+
+import bg.an.englishacademy.model.binding.LessonAddBindingModel;
+import bg.an.englishacademy.model.binding.LessonEditBindingModel;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 public interface LessonValidationService {
+
+    boolean lessonAddHasErrors(LessonAddBindingModel lessonAddBindingModel, BindingResult bindingResult, RedirectAttributes redirectAttributes);
 }
