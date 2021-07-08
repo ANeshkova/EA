2f9e6a40cf42bfced69e4140d4edaae9276a60b6
ANeshkova <anitowurth@abv.bg>
2021-07-08 10:09:47 +0300

    Implemented "addLesson" in LessonServiceImpl

--- src/main/java/bg/an/englishacademy/service/impl/LessonServiceImpl.java
+++ src/main/java/bg/an/englishacademy/service/impl/LessonServiceImpl.java
@@ -1,4 +1,29 @@
 package bg.an.englishacademy.service.impl;
+
+import bg.an.englishacademy.model.entity.LessonEntity;
+import bg.an.englishacademy.model.service.LessonServiceModel;
+import bg.an.englishacademy.repository.LessonRepository;
+import bg.an.englishacademy.service.LessonService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
 @Service
 public class LessonServiceImpl implements LessonService {
+
+    private final LessonRepository lessonRepository;
+    private final ModelMapper modelMapper;
+
+    public LessonServiceImpl(LessonRepository lessonRepository, ModelMapper modelMapper) {
+        this.lessonRepository = lessonRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public LessonServiceModel addLesson(LessonServiceModel lessonServiceModel) {
+        LessonEntity lessonEntity = this.modelMapper.map(lessonServiceModel, LessonEntity.class);
+        lessonEntity.setAddedOn(LocalDate.now());
+
+        this.lessonRepository.save(lessonEntity);
+        return lessonServiceModel;
+    }
 }
