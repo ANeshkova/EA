2f522125d956f4f83b6949bf6979427b005c9f9e
ANeshkova <anitowurth@abv.bg>
2021-07-11 10:36:23 +0300

    Added "findAllLessons" in LessonService

--- src/main/java/bg/an/englishacademy/service/LessonService.java
+++ src/main/java/bg/an/englishacademy/service/LessonService.java
@@ -2,8 +2,12 @@ package bg.an.englishacademy.service;
 
 import bg.an.englishacademy.model.service.LessonServiceModel;
 
+import java.util.List;
+
 public interface LessonService {
     LessonServiceModel addLesson(LessonServiceModel lessonServiceModel);
 
     boolean lessonTitleExists(String title);
+
+    List<LessonServiceModel> findAllLessons();
 }
