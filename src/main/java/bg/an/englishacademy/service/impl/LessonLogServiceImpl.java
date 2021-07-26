2ad50fdb7a818cc9fc0eec57cdc0124f8fd58cde
ANeshkova <anitowurth@abv.bg>
2021-07-26 21:18:13 +0300

    Added LessonLogServiceImpl

---
+++ src/main/java/bg/an/englishacademy/service/impl/LessonLogServiceImpl.java
@@ -0,0 +1,6 @@
+package bg.an.englishacademy.service.impl;
+import bg.an.englishacademy.service.LessonLogService;
+import org.springframework.stereotype.Service;
+@Service
+public class LessonLogServiceImpl implements LessonLogService {
+}
