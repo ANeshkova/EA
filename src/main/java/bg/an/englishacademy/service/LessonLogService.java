8bc3f34e1163d3223cc69c26532c78326441f942
ANeshkova <anitowurth@abv.bg>
2021-07-27 23:24:27 +0300

    Added "findAllLogs" in LessonLogService

--- src/main/java/bg/an/englishacademy/service/LessonLogService.java
+++ src/main/java/bg/an/englishacademy/service/LessonLogService.java
@@ -1,6 +1,12 @@
 package bg.an.englishacademy.service;
 
+import bg.an.englishacademy.model.service.LessonLogServiceModel;
+
+import java.util.List;
+
 public interface LessonLogService {
 
     void createLog(String action, Long lessonId);
+
+    List<LessonLogServiceModel> findAllLogs();
 }
