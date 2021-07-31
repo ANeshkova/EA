7bea0c5833b943ffa954f3a61ace26f3b901a16d
ANeshkova <anitowurth@abv.bg>
2021-07-31 14:09:42 +0300

    Added CategoryLogServiceImpl

---
+++ src/main/java/bg/an/englishacademy/service/impl/CategoryLogServiceImpl.java
@@ -0,0 +1,6 @@
+package bg.an.englishacademy.service.impl;
+import bg.an.englishacademy.service.CategoryLogService;
+import org.springframework.stereotype.Service;
+@Service
+public class CategoryLogServiceImpl implements CategoryLogService {
+}
