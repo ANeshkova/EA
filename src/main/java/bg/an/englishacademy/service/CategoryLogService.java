78a615c6e192fad7453d37a51d2f0c8fb0bd65ce
ANeshkova <anitowurth@abv.bg>
2021-08-01 15:18:33 +0300

    Added "findAllLogs" in CategoryLogService

--- src/main/java/bg/an/englishacademy/service/CategoryLogService.java
+++ src/main/java/bg/an/englishacademy/service/CategoryLogService.java
@@ -1,6 +1,12 @@
 package bg.an.englishacademy.service;
 
+import bg.an.englishacademy.model.service.CategoryLogServiceModel;
+
+import java.util.List;
+
 public interface CategoryLogService {
 
     void createLog(String action, String categoryName);
+
+    List<CategoryLogServiceModel> findAllLogs();
 }
