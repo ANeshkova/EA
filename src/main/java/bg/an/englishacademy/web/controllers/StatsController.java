f7bf5111f9994a3272ce4b1dd34c3729eab6af17
ANeshkova <anitowurth@abv.bg>
2021-08-02 14:25:10 +0300

    Added categoryLog in StatsController

--- src/main/java/bg/an/englishacademy/web/controllers/StatsController.java
+++ src/main/java/bg/an/englishacademy/web/controllers/StatsController.java
@@ -1,5 +1,6 @@
 package bg.an.englishacademy.web.controllers;
 
+import bg.an.englishacademy.service.CategoryLogService;
 import bg.an.englishacademy.service.LessonLogService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -11,15 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/statistics")
 public class StatsController {
 
+    private final CategoryLogService categoryLogService;
     private final LessonLogService lessonLogService;
 
-    public StatsController(LessonLogService lessonLogService) {
+    public StatsController(CategoryLogService categoryLogService, LessonLogService lessonLogService) {
+        this.categoryLogService = categoryLogService;
         this.lessonLogService = lessonLogService;
     }
 
     @GetMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String stats(Model model){
+        model.addAttribute("categoryLogs", categoryLogService.findAllLogs());
         model.addAttribute("lessonLogs", lessonLogService.findAllLogs());
         return "stats";
     }
